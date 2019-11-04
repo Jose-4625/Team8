5b69afbb4472be57c55e1588249ac25f39be9eaf
Angelia Wu <42920321+angeliawu@users.noreply.github.com>
2019-11-03 23:34:15 -0600

    options

--- src/scenes/OptionsScene.js
+++ src/scenes/OptionsScene.js
@@ -18,11 +18,11 @@ export default class OptionsScene extends Phaser.Scene
     this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
     this.load.audio('InGame','./assets/sounds/InGame2.wav');
     this.load.audio("pausefx","./assets/sounds/Kindlich Text.mp3");
-    this.load.image("optionsTitle", "./assets/fullSized/Options Title.png");
+    this.load.image("storeback", "./assets/fullSized/Store Back.png");
 
     //Declare variables for center of the scene
     this.centerX = this.cameras.main.width/2;
-    this.centerY=this.cameras.main.height/2;
+    this.centerY = this.cameras.main.height/2;
   }
 
   create()
@@ -37,64 +37,77 @@ export default class OptionsScene extends Phaser.Scene
       volume:.3,
       loop:true
     });
-    this.pausefx=this.sound.add('pausefx');
-    //Create the scenes
+    this.pausefx = this.sound.add('pausefx');
+
+    //Load fonts
     WebFont.load
     ({
-      google:{
-        families: ['Permanent Marker', 'Modak', 'Anton', 'Amatic SC']
+      google:
+      {
+        families: ['Permanent Marker', 'Modak', 'Anton', 'Neucha']
       }
     });
-
-    //Background
-    this.cameras.main.setBackgroundColor(0xffe6cc);
-
-    //Display title
-    this.add.sprite(400, 150, "optionsTitle").setScale(0.3);
+    
+    //Display background
+    this.add.sprite(400, 280, 'storeback').setScale(0.6);
 
     //Display text
-    var text = 'Press Enter to return to game.\n' +
-               'Press F11 to full screen. \n' +
-               'Press ESC to return to main menu.\n' +
-               'Press R to restart level.';
-    this.spellOutText(175, 250, 550, text, 50, 20, '#000000', 'Amatic SC');
+    var text1 = 'Enter: Return to game\n' +
+                'F11: Full screen';
+    var text2 = 'ESC: Main menu\n' +
+                'R: Restart level';
+    this.spellOutText(100, 285, 550, text1, 40, 20, '#000000', 'Neucha');
+    this.spellOutText(450, 285, 550, text2, 40, 20, '#000000', 'Neucha');
   }
 
-  spellOutText(x, y, width, text, fontSize, speed, fill, font){
-    var sentence = this.add.text(x,y, "", {
-      fontSize: fontSize,
-      fill: fill,
-      fontFamily: font
-    });
-    var currentLine = this.add.text(10,10,"", {
-      fontSize:fontSize,
-      fontFamily: font
-    });
-    currentLine.alpha=0;
+  //Gradually spells text out
+  spellOutText(x, y, width, text, fontSize, speed, fill, font)
+  {
+    var sentence = this.add.text
+    (x, y, "",
+      {
+        fontSize: fontSize,
+        fill: fill,
+        fontFamily: font
+      }
+    );
+    var currentLine = this.add.text
+    (10, 10, "",
+      {
+        fontSize:fontSize,
+        fontFamily: font
+      }
+    );
+    currentLine.alpha = 0;
     var index = 0;
     var timer;
 
     //Start the text loop
     startLoop(this);
 
-    function startLoop(that){
-      timer = that.time.addEvent({
+    function startLoop(that)
+    {
+      timer = that.time.addEvent
+      ({
         delay: speed,
         callback: addChar,
         callbackScope: this,
         loop: true
       });
 
-      function addChar(){
+      function addChar()
+      {
         sentence.text += text[index];
         currentLine.text += text[index];
 
-        if (currentLine.width > width && text[index] === " "){
+        if (currentLine.width > width && text[index] === " ")
+        {
           sentence.text +=  "\n";
           currentLine.text = "";
         }
 
-        if (index >= text.length - 1){
+        if (index >= text.length - 1)
+        {
           timer.remove();
           console.log("stop");
         }
