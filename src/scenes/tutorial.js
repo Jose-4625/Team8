ba296299068468afec7fac32888ad0c10bbcbf1b
Angelia Wu <42920321+angeliawu@users.noreply.github.com>
2019-12-09 20:18:17 -0600

    shat

--- src/scenes/tutorial.js
+++ src/scenes/tutorial.js
@@ -1,61 +1,42 @@
-/*global Phaser*/
 import DefaultScene from './DefaultScene.js';
 
-export default class Tutorial extends DefaultScene {
-  constructor () {
+export default class Tutorial extends DefaultScene
+{
+  constructor()
+  {
     super('tutorial');
   }
-  init(data){
-    //super.import_test();
+  init(data)
+  {
     return super.init({level:'tutorial'});
-
   }
-  preload(){
-    //this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js');
+  preload()
+  {
+    //Load the tutorial video
+    this.load.video("TutorialVideo", './assets/Fried or Flight Tutorial.mp4');
+
     this.mapKey = 'tutorial';
     this.mapPath = "./assets/tilemaps/tutorial.json";
     return super.preload(this.mapKey, this.mapPath);
-
   }
-  create(){
+  create()
+  {
+    //Display video
+    this.TutorialVideo = this.add.video(1695, 555, "TutorialVideo").setScale(0.2);
+    this.TutorialVideo.play();
+
+    //Check when video stops
+    var isPlaying = this.TutorialVideo.isPlaying();
+    if (!isPlaying)
+    {
+      this.TutorialVideo.destroy();
+    }
+
     this.dataStore = {"key": "Value"};
-    
-    var text = this.add.text(1500, 400, 'The only controls are\nthe arrows keys\nfor their respective\ndirections.' ,{
-      font: "24px monospace",
-      fill: "#00ffee",
-      padding: { x: 20, y: 20 }
-    });
-    var text = this.add.text(1500, 360, 'Can be moved-- Okay to touch --slip if touched' ,{
-      font: "14px monospace",
-      fill: "#00ffee",
-      padding: { x: 20, y: 20 }
-    });
-    var text = this.add.text(1500, 700, '(Varies in Size)' ,{
-      font: "12px monospace",
-      fill: "#00ffee",
-      padding: { x: 20, y: 20 }
-    });
-    var text = this.add.text(1500, 720, 'will chase you-- Do not touch --bad for produce' ,{
-      font: "12px monospace",
-      fill: "#00ffee",
-      padding: { x: 20, y: 20 }
-    });
-    var text = this.add.text(1280, 250, 'Immoveable' ,{
-      font: "24px monospace",
-      fill: "#00ffee",
-      padding: { x: 20, y: 20 }
-    });
-
-
-
     return super.create(this.mapKey, false);
-
-
   }
-  update(){
+  update()
+  {
     return super.update('level1');
-
-
   }
-
 }
